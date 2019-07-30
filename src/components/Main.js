240097a9106f7296c54b80b996bd90732d286cd1
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2019-07-30 01:15:35 -0700

    comment out pictures for now

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,52 +1,123 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
+// import pic01 from '../images/pic01.jpg'
+// import pic02 from '../images/pic02.jpg'
+// import pic03 from '../images/pic03.jpg'
 
 class Main extends React.Component {
   render() {
-
-    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+    let close = (
+      <div
+        className="close"
+        onClick={() => {
+          this.props.onCloseArticle()
+        }}
+      />
+    )
 
     return (
-      <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
-
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+      <div
+        ref={this.props.setWrapperRef}
+        id="main"
+        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+      >
+        <article
+          id="intro"
+          className={`${this.props.article === 'intro' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>I make things. Not this website though. This was made with an awesome <a href="https://github.com/codebushi/gatsby-starter-dimension-codebushi">template</a> that works with GatsbyJS and deployed to GitHub pages. Check out my work in the work tab.</p>
-          <p>Fun fact: The background image was made in GIMP (blending two existing images).</p>
+          {/* <span className="image main"><img src={pic01} alt="" /></span> */}
+          <p>
+            I make things. Not this website though. This was made with an
+            awesome{' '}
+            <a href="https://github.com/codebushi/gatsby-starter-dimension-codebushi">
+              template
+            </a>{' '}
+            that works with GatsbyJS and deployed to GitHub pages. Check out my
+            work in the work tab.
+          </p>
+          <p>
+            Fun fact: The background image was made in GIMP (blending two
+            existing images).
+          </p>
           {close}
         </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article
+          id="work"
+          className={`${this.props.article === 'work' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>I'm currently working on <a href="https://callpepper.co">Pepper</a>, a digital receptionist that blocks spam and robocalls before they get to you.</p>
-          <p>I've made some other stuff for fun too. Find that on my <a href="https://github.com/aniravi24">GitHub</a>.</p>
-          <p>Find the work I've done on my <a href="https://www.linkedin.com/in/aniravichandran">LinkedIn</a>.</p>
+          {/* <span className="image main"><img src={pic02} alt="" /></span> */}
+          <p>
+            I'm currently working on <a href="https://callpepper.co">Pepper</a>,
+            a digital receptionist that blocks spam and robocalls before they
+            get to you.
+          </p>
+          <p>
+            I've made some other stuff for fun too. Find that on my{' '}
+            <a href="https://github.com/aniravi24">GitHub</a>.
+          </p>
+          <p>
+            Find the work I've done on my{' '}
+            <a href="https://www.linkedin.com/in/aniravichandran">LinkedIn</a>.
+          </p>
           {close}
         </article>
 
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article
+          id="about"
+          className={`${this.props.article === 'about' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">About</h2>
-          <span className="image main"><img src={pic03} alt="" /></span>
+          {/* <span className="image main"><img src={pic03} alt="" /></span> */}
           <p>You saw my intro right? I'm just a guy.</p>
           {close}
         </article>
 
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article
+          id="contact"
+          className={`${this.props.article === 'contact' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Contact</h2>
           <ul className="icons">
-            <li><a href="https://www.linkedin.com/in/aniravichandran" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
-            <li><a href="https://github.com/aniravi24" className="icon fa-github"><span className="label">GitHub</span></a></li>
-            <li><a href="https://twitter.com/therealAniRavi" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/aniravichandran"
+                className="icon fa-linkedin"
+              >
+                <span className="label">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/aniravi24" className="icon fa-github">
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/therealAniRavi"
+                className="icon fa-twitter"
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
           </ul>
           {close}
         </article>
-
       </div>
     )
   }
