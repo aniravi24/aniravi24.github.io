db6ce5343dc46c9dd99770211c289e2b7c389038
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2020-01-19 15:24:54 -0700

    improve meta tags

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,21 +6,14 @@ import { StaticQuery, graphql } from 'gatsby'
 import '../assets/scss/main.scss'
 
 const Layout = ({ children, location }) => {
-
-  let content;
+  let content
 
   if (location && location.pathname === '/') {
-    content = (
-      <div>
-        {children}
-      </div>
-    )
+    content = <div>{children}</div>
   } else {
     content = (
       <div id="wrapper" className="page">
-        <div>
-          {children}
-        </div>
+        <div>{children}</div>
       </div>
     )
   }
@@ -41,8 +34,11 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'Just a guy.' },
+              {
+                name: 'keywords',
+                content: 'technology,guy,ani,ravi,anirudh,ravichandran',
+              },
             ]}
           >
             <html lang="en" />
