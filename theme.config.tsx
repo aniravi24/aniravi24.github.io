8630549756d3da62abe577e9b87e60132e2d470a
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2024-10-16 17:17:04 -0400

    fix head tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,16 +1,31 @@
 import React from "react";
 import { NextraBlogTheme } from "nextra-theme-blog";
 
+const defaultMeta = {
+  title: "Ani Ravi",
+  description: "Ani Ravi's personal website.",
+};
+
 // eslint-disable-next-line import/no-anonymous-default-export
 const config: NextraBlogTheme = {
   comments: <></>,
   head: ({ title, meta }) => (
     <>
-      {meta.description && (
-        <meta name="description" content={meta.description} />
-      )}
+      <meta
+        name="description"
+        content={meta.description || defaultMeta.description}
+      />
       {meta.tag && <meta name="keywords" content={meta.tag} />}
       {meta.author && <meta name="author" content={meta.author} />}
+      <meta
+        property="og:site_name"
+        content={meta.title || title || defaultMeta.title}
+      />
+      <meta
+        property="og:description"
+        content={meta.description || defaultMeta.description}
+      />
+      <meta property="og:title" content={meta.title || defaultMeta.title} />
     </>
   ),
   footer: (
