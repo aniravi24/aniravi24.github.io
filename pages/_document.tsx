78c0159d95cfeee9c47598e27d438fbd9e2e8b91
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2024-10-16 17:17:04 -0400

    fix head tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  const meta = {
-    title: "Ani Ravi",
-    description: "Ani Ravi's personal website.",
-  };
-
   return (
     <Html lang="en">
       <Head>
         <meta name="robots" content="follow, index" />
-        <meta name="description" content={meta.description} />
-        <meta property="og:site_name" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
 
         {/* Favicon theming */}
         <link
