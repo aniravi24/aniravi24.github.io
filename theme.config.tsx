5c1c96381c9c238e399515f1b2ff5844c17954fa
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2024-10-16 17:09:02 -0400

    attempt to have custom metadata per page

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,6 +4,15 @@ import { NextraBlogTheme } from "nextra-theme-blog";
 // eslint-disable-next-line import/no-anonymous-default-export
 const config: NextraBlogTheme = {
   comments: <></>,
+  head: ({ title, meta }) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
   footer: (
     <div className="my-4 border-t-2 mx-auto flex items-center sm:flex-row flex-col">
       <p className="text-sm text-gray-500">Ani Ravi</p>
